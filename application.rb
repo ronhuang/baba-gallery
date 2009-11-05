eaa08a60848421a3ab71307963ea0771c19701f0
Ron Huang <ronhuang@gmail.com>
2009-11-05 15:06:08 +0800

    Added various url to operate on artworks.

--- application.rb
+++ application.rb
@@ -16,7 +16,55 @@ helpers do
   # add your helpers here
 end
 
-# root page
-get '/' do
-  haml :root
+# Return list of all installed artworks.
+# Just get all artworks and return as JSON.
+get '/artworks' do
+  content_type 'application/json'
+  { 'content' => Array(Artwork.all) }.to_json
+end
+
+# Create a new artwork. Request body to contain json
+post '/artworks' do
+  opts = Artwork.parse_json(request.body.read) rescue nil
+  halt(401, 'Invalid Format') if opts.nil?
+
+  artwork = Artwork.new(opts)
+  halt(500, 'Could not save artwork') unless artwork.save
+
+  response['Location'] = artwork.url
+  response.status = 201
+end
+
+# Get an individual artwork
+get "/artworks/:id" do
+  artwork = Artwork.get(params[:id]) rescue nil
+  halt(404, 'Not Found') if artwork.nil?
+
+  content_type 'application/json'
+  { 'content' => artwork }.to_json
+end
+
+# Update an individual artwork
+put "/artworks/:id" do
+  artwork = Artwork.get(params[:id]) rescue nil
+  halt(404, 'Not Found') if artwork.nil?
+
+  opts = Artwork.parse_json(request.body.read) rescue nil
+  halt(401, 'Invalid Format') if opts.nil?
+
+  artwork.name = opts[:name]
+  artwork.email = opts[:email]
+  artwork.created_at = opts[:created_at]
+  artwork.view_count = opts[:view_count]
+  artwork.vote_count = opts[:vote_count]
+  artwork.save
+
+  response['Content-Type'] = 'application/json'
+  { 'content' => artwork }.to_json
+end
+
+# Delete an invidual artwork
+delete '/artworks/:id' do
+  artwork = Artwork.get(params[:id]) rescue nil
+  artwork.destroy unless artwork.nil?
 end
