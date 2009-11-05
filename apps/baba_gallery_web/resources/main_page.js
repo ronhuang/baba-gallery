449cb1cad0f4c873c7078f10bea96041d9dbf68e
Ron Huang <ronhuang@gmail.com>
2009-11-05 21:09:56 +0800

    Changed layout for main page.

--- apps/baba_gallery_web/resources/main_page.js
+++ apps/baba_gallery_web/resources/main_page.js
@@ -4,20 +4,53 @@
 // ==========================================================================
 /*globals BabaGalleryWeb */
 
-// This page describes the main user interface for your application.  
+// This page describes the main user interface for your application.
 BabaGalleryWeb.mainPage = SC.Page.design({
 
   // The main pane is made visible on screen as soon as your app is loaded.
-  // Add childViews to this pane for views to display immediately on page 
+  // Add childViews to this pane for views to display immediately on page
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
-    
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
-      value: "Welcome to SproutCore!"
+    childViews: 'middleView topView bottomView'.w(),
+
+    topView: SC.ToolbarView.design({
+      layout: { top: 0, left: 0, right: 0, height: 36 },
+      childViews: 'labelView addButton'.w(),
+      anchorLocation: SC.ANCHOR_TOP,
+
+      labelView: SC.LabelView.design({
+        layout: { centerY: 0, height: 24, left: 8, width: 200 },
+        controlSize: SC.LARGE_CONTROL_SIZE,
+        fontWeight: SC.BOLD_WEIGHT,
+        value:   'Baba Gallery'
+      }),
+
+      addButton: SC.ButtonView.design({
+        layout: { centerY: 0, height: 24, right: 12, width: 100 },
+        title:  "Add Artwork"
+      })
+    }),
+
+    middleView: SC.ScrollView.design({
+      hasHorizontalScroller: NO,
+      layout: { top: 36, bottom: 32, left: 0, right: 0 },
+      backgroundColor: 'white',
+
+      contentView: SC.ListView.design({
+      })
+    }),
+
+    bottomView: SC.ToolbarView.design({
+      layout: { bottom: 0, left: 0, right: 0, height: 32 },
+      childViews: 'summaryView'.w(),
+      anchorLocation: SC.ANCHOR_BOTTOM,
+
+      summaryView: SC.LabelView.design({
+        layout: { centerY: 0, height: 18, left: 20, right: 20 },
+        textAlign: SC.ALIGN_CENTER,
+
+        value: "Item Count"
+      })
     })
   })
 
